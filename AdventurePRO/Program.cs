9b73020f90441f9b306729fdb4914f090f986576
SharonDaimon <sharondaimon@yandex.ru>
2016-02-28 00:37:52 +0300

    new three projects

---
+++ AdventurePRO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventurePRO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program starts with the idea");
+            Console.ReadKey();
+        }
+    }
+}
